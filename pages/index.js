01fcd1859e29464d147a59ef46a132990f689529
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-10-14 17:01:36 +0500

    feat: mobile version

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,89 @@
-const swiper = new Swiper('.swiper', {
+const individualShotsSizeBigSwiper = new Swiper('.individual-shoots__swiper_size-big', {
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    nextEl: '.individual-shoots__swiper_size-big-button-next',
+    prevEl: '.individual-shoots__swiper_size-big-button-prev'
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.individual-shoots__swiper_size-big-pagination',
     clickable: true,
   },
   loop: true,
-  spaceBetween: 30,
 
-  autoplay: {
-    delay: 3000,
 
-    stopOnLastSlide: false,
+  // autoplay: {
+  //   delay: 3000,
 
-    disableOnInteractions: false
-  }
+  //   stopOnLastSlide: false,
+
+  //   disableOnInteractions: true
+  // }
 });
+
+const individualShotsSizeSmallSwiper = new Swiper('.individual-shoots__swiper_size-small', {
+  navigation: {
+    nextEl: '.individual-shoots__swiper_size-small-button-next',
+    prevEl: '.individual-shoots__swiper_size-small-button-prev'
+  },
+  pagination: {
+    el: '.individual-shoots__swiper_size-small-pagination',
+    clickable: true,
+  },
+  loop: true,
+
+
+  // autoplay: {
+  //   delay: 3000,
+
+  //   stopOnLastSlide: false,
+
+  //   disableOnInteractions: true
+  // }
+});
+
+const aboutShotsSizeBigSwiper = new Swiper('.about-shoots__swiper_size-big', {
+  navigation: {
+    nextEl: '.about-shoots__swiper_size-big-button-next',
+    prevEl: '.about-shoots__swiper_size-big-button-prev'
+  },
+  pagination: {
+    el: '.about-shoots__swiper_size-big-pagination',
+    clickable: true,
+  },
+  loop: true,
+
+
+  // autoplay: {
+  //   delay: 3000,
+
+  //   stopOnLastSlide: false,
+
+  //   disableOnInteractions: true
+  // }
+});
+
+const aboutShotsSizeSmallSwiper = new Swiper('.about-shoots__swiper_size-small', {
+  navigation: {
+    nextEl: '.about-shoots__swiper_size-small-button-next',
+    prevEl: '.about-shoots__swiper_size-small-button-prev'
+  },
+  pagination: {
+    el: '.about-shoots__swiper_size-small-pagination',
+    clickable: true,
+  },
+  loop: true,
+
+
+  // autoplay: {
+  //   delay: 3000,
+
+  //   stopOnLastSlide: false,
+
+  //   disableOnInteractions: true
+  // }
+});
+
+individualShotsSizeBigSwiper.controller.control = individualShotsSizeSmallSwiper;
+individualShotsSizeSmallSwiper.controller.control = individualShotsSizeBigSwiper;
+
+aboutShotsSizeBigSwiper.controller.control = aboutShotsSizeSmallSwiper;
+aboutShotsSizeSmallSwiper.controller.control = aboutShotsSizeBigSwiper;
